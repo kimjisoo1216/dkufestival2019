42311aa2dc60912e64881a118d69614767c93f2a
kimjisoo <kimjisoo1216@naver.com>
2019-05-14 04:06:07 +0900

    first commit

--- festival/settings.py
+++ festival/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&6=gu&!lgeav^s7_)o16e0ik8frulc5&4mub+pdt$ekdbu!8)o'
+#SECRET_KEY = '&6=gu&!lgeav^s7_)o16e0ik8frulc5&4mub+pdt$ekdbu!8)o'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = []
 
